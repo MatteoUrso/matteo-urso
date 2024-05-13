a8a680735093460902f10ffe979ce09dbf046d4d
Matteo Urso <matteourso100@gmail.com>
2024-05-13 15:25:33 +0200

    init clerk

---
+++ app/not-found.tsx
@@ -0,0 +1,20 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "404 | Brian Ruiz",
+  description: "Uh oh! This page does not exist",
+};
+
+export default function NotFound() {
+  <div className="flex flex-col gap-2">
+    <h1 className="text-3xl font-bold tracking-tight text-primary">404</h1>
+    <p className="text-secondary">
+      Uh oh! This page does not exists, maybe you clicked an old link or
+      misspelled. Please try again…
+    </p>
+    <div className="h-2" />
+    {/* <Link href="/" underline>
+      Return home
+    </Link> */}
+  </div>;
+}
